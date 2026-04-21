21ba6b6012bc8e845309887c41f1cb8506f8c208
K. Gadberry <1781520+kgadberry@users.noreply.github.com>
2026-04-21 03:24:56 +0000

    added openssl, libargon2 and opentofu

--- modules/common/shell/utils.nix
+++ modules/common/shell/utils.nix
@@ -18,30 +18,33 @@ in {
         home-manager.users.${config.user} = {
 
             home.packages = with pkgs; [
-                age # Encryption
-                bc # Calculator
-                dig # DNS lookup
-                fd # find
-                htop # Show system processes
-                inetutils # Includes telnet, whois
-                jq # JSON manipulation
-                lf # File viewer
-                qrencode # Generate qr codes
-                rsync # Copy folders
-                ripgrep # grep
-                sd # sed
-                tealdeer # Cheatsheets
-                tree     # View directory hierarchy
-                zip      # Create zips
-                unzip    # Extract zips
-                broot    # TUI file manager
-                neofetch # System info
-                wget     # Download files
-                mc       # Better than termscp
-                dos2unix # Convert line endings
-                usbutils # For lsusb, etc
-                nmap     # Network exploration
-                tcpdump  # Packet sniffer
+                age         # Encryption
+                bc          # Calculator
+                dig         # DNS lookup
+                fd          # find
+                htop        # Show system processes
+                inetutils   # Includes telnet, whois
+                jq          # JSON manipulation
+                lf          # File viewer
+                qrencode    # Generate qr codes
+                rsync       # Copy folders
+                ripgrep     # grep
+                sd          # sed
+                tealdeer    # Cheatsheets
+                tree        # View directory hierarchy
+                zip         # Create zips
+                unzip       # Extract zips
+                broot       # TUI file manager
+                neofetch    # System info
+                wget        # Download files
+                mc          # Better than termscp
+                dos2unix    # Convert line endings
+                usbutils    # For lsusb, etc
+                nmap        # Network exploration
+                tcpdump     # Packet sniffer
+                openssl     # Cryptographic tools
+                libargon2   # argon2 hash tool
+                opentofu    # Terraform-compatible
             ];
 
             programs.zoxide.enable = true; # Shortcut jump command
